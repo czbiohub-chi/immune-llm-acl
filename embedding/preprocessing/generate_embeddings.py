acbff268cf88e2f2adb279550ed29bae40e199f3
Steven Song <steven.song98@gmail.com>
2025-04-30 13:08:07 -0500

    inference notebook

--- embedding/preprocessing/generate_embeddings.py
+++ embedding/preprocessing/generate_embeddings.py
@@ -31,27 +31,48 @@ def worker(batch: list[str], client: OpenAI, model: str) -> list[list[float]]:
     return [d.embedding for d in out.data]
 
 
-def main(args):
-    assert not os.path.exists(args.output_file)
-    client = OpenAI(base_url=args.api_url, api_key=args.api_key)
-
-    input_df = pd.read_csv(args.input_file)
-    terms = input_df[args.input_col]
+def generate_embeddings(
+    *,  # enforce kwargs
+    terms: list[str],
+    api_url: str,
+    api_key: str,
+    model: str,
+    num_workers: int = 0,
+    batch_size: int = 1024,
+) -> list[np.ndarray]:
+    client = OpenAI(base_url=api_url, api_key=api_key)
     batched_terms = [
-        terms[i : i + args.batch_size] for i in range(0, len(terms), args.batch_size)
+        terms[i : i + batch_size] for i in range(0, len(terms), batch_size)
     ]
-    if args.num_workers == 0:
+    if num_workers == 0:
         batched_embeddings: list[list[list[float]]] = []
         for batch in tqdm(batched_terms):
-            batched_embeddings.append(worker(batch, client, args.model))
+            batched_embeddings.append(worker(batch, client, model))
     else:
         batched_embeddings = pqdm(
             batched_terms,
-            lambda batch: worker(batch, client, args.model),
-            n_jobs=args.num_workers,
+            lambda batch: worker(batch, client, model),
+            n_jobs=num_workers,
         )
-
     embeddings = [np.asarray(e) for batch in batched_embeddings for e in batch]
+    return embeddings
+
+
+def main(args):
+    assert not os.path.exists(args.output_file)
+
+    input_df = pd.read_csv(args.input_file)
+    terms = input_df[args.input_col]
+
+    embeddings = generate_embeddings(
+        terms=terms,
+        api_url=args.api_url,
+        api_key=args.api_key,
+        model=args.model,
+        num_workers=args.num_workers,
+        batch_size=args.batch_size,
+    )
+
     embeddings = {k: v for k, v in zip(input_df[args.output_col], embeddings)}
     np.save(args.output_file, embeddings)
 
--- embedding/preprocessing/generate_summaries.py
+++ embedding/preprocessing/generate_summaries.py
@@ -16,7 +16,7 @@ def parse_args():
     parser.add_argument("--output_file", required=True)
     parser.add_argument("--api_url", default="http://localhost:8000/v1")
     parser.add_argument("--api_key", default="EMPTY")
-    parser.add_argument("--model")
+    parser.add_argument("--model", required=True)
     parser.add_argument("--num_workers", default=4, type=int)
     args = parser.parse_args()
     return args
@@ -35,26 +35,46 @@ def worker(term, prompt_template, client, model):
     return completion.choices[0].message.content
 
 
-def main(args):
-    client = OpenAI(base_url=args.api_url, api_key=args.api_key)
-    if args.model is None:
-        args.model = client.models.list().data[0].id
+def generate_summaries(
+    *,  # enforce kwargs
+    terms: list[str],
+    prompt_file: str,
+    api_url: str,
+    api_key: str,
+    model: str,
+    num_workers: int = 0,
+) -> list[str]:
+    client = OpenAI(base_url=api_url, api_key=api_key)
 
-    with open(args.prompt_file) as f:
+    with open(prompt_file) as f:
         prompt_template = f.read()
 
-    input_df = pd.read_csv(args.input_file)
-    terms = input_df[args.input_col]
-    if args.num_workers == 0:
+    if num_workers == 0:
         summaries = []
         for term in tqdm(terms):
-            summaries.append(worker(term, prompt_template, client, args.model))
+            summaries.append(worker(term, prompt_template, client, model))
     else:
         summaries = pqdm(
             terms,
-            lambda term: worker(term, prompt_template, client, args.model),
-            n_jobs=args.num_workers,
+            lambda term: worker(term, prompt_template, client, model),
+            n_jobs=num_workers,
         )
+
+    return summaries
+
+
+def main(args):
+    input_df = pd.read_csv(args.input_file)
+    terms = input_df[args.input_col]
+
+    summaries = generate_summaries(
+        terms=terms,
+        prompt_file=args.prompt_file,
+        api_url=args.api_url,
+        api_key=args.api_key,
+        model=args.model,
+        num_workers=args.num_workers,
+    )
 
     input_df[args.input_col + "_summarized"] = summaries
     input_df.to_csv(args.output_file, index=False)
